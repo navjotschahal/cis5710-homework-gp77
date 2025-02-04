1b77885bf5764861b13a93698571ba0ae61a0a7e
adamt38 <adam.thomson38@gmail.com>
2025-02-04 19:05:47 +0000

    draft one of gp4

--- hw2b-cla/cla.sv
+++ hw2b-cla/cla.sv
@@ -26,7 +26,14 @@ module gp4(input wire [3:0] gin, pin,
            output wire gout, pout,
            output wire [2:0] cout);
 
-   // TODO: your code here
+   // Compute carry-out signals
+    assign cout[0] = gin[0] | (pin[0] & cin);
+    assign cout[1] = gin[1] | (pin[1] & cout[0]);
+    assign cout[2] = gin[2] | (pin[2] & cout[1]);
+    
+    // Compute propagate and generate signals
+    assign pout = &pin; // Equivalent to (pin[3] & pin[2] & pin[1] & pin[0])
+    assign gout = gin[3] | (pin[3] & gin[2]) | (pin[3] & pin[2] & gin[1]) | (pin[3] & pin[2] & pin[1] & gin[0])
 
 endmodule
 
