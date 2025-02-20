b1192e3e66ac032dfb4a69dfb0918e25eb7fa7b5
Navjot Singh Chahal <navjotsinghchahal@Outlook.com>
2025-02-20 16:54:03 -0500

    work so far

--- hw2b-cla/system/System_original.sv
+++ hw2b-cla/system/System_original.sv
@@ -1,22 +1,7 @@
-`timescale 1ns/1ps
 `include "cla.sv"
-
 module SystemDemo(input wire [6:0] btn,
                  output wire [7:0] led);
    wire [31:0] sum;
-//    reg [6:0] btn_reg;
-
-    integer i;
-    initial begin
-        btn = 6'b0000000;
-        for (i = 0; i < 128; i = i + 1) begin
-        #10000 btn = i[6:0];
-        end
-    end
-
    cla cla_inst(.a(32'd26), .b({26'b0, btn[1], btn[2], btn[3], btn[5], btn[4], btn[6]}), .cin(1'b0), .sum(sum));
    assign led = sum[7:0];
-
-  
-
 endmodule
